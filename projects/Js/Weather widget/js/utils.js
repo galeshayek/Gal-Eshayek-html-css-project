e82df3802cbed370ce039b2e14f4b5812bb55c17
galeshayek <galeshayek15@gmail.com>
2024-02-28 20:39:58 +0200

    organization

---
+++ projects/Js/Weather widget/js/utils.js
@@ -0,0 +1,38 @@
+const temp = document.querySelector('.temperature');
+const location = document.querySelector('.location');
+const date = new Date;
+const utcHour = date.getUTCHours();
+export function updateWeatherIcon(weatherDescription, utcDifference) {
+    const weatherIcon = document.getElementById("weather-icon");
+    const hour = utcHour + utcDifference / 3600;
+    const isDayTime = hour < 17 && hour > 6;
+    const icons = {
+        Clear: isDayTime ? '☀️' : '🌕',
+        Clouds: '☁️',
+        Atmosphere: '💨',
+        Snow: '❄️',
+        Rain: '🌧️',
+        Drizzle: '💦',
+        Thunderstorm: '⛈️',
+    };
+    weatherIcon.textContent = icons[weatherDescription] || '';
+}
+
+export const enterToSearch = document.getElementById('searchBox').addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        document.getElementById("search").click();
+    }
+})
+
+export function fetchWeather(url) {
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            const weatherDescription = data.weather[0].main;
+            temp.textContent = Math.round(data.main.temp) + '°C';
+            location.textContent = data.name;
+            updateWeatherIcon(weatherDescription, data.timezone);
+        })
+        .catch(error => console.log(error));
+}
