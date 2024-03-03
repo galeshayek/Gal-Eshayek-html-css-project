ae5a79ed40a821c2c3d78dac99ee52fc51068a19
galeshayek <galeshayek15@gmail.com>
2024-03-03 19:12:14 +0200

    world clock

--- projects/Js/world-clock/js/general.js
+++ projects/Js/world-clock/js/general.js
@@ -1,16 +1,21 @@
+const main = document.getElementById("main");
 const timeIsrael = document.getElementById("timeIsrael");
 const timeUk = document.getElementById("timeUk");
 const timeUs = document.getElementById("timeUs");
 const timeIndia = document.getElementById("timeIndia");
-
+const timeFormat = document.getElementById("format");
+let format = false;
+timeFormat.addEventListener('change', () => {
+    (timeFormat.value === "24") ? format = false : format = true;
+})
 
 setInterval(() => {
     const d = new Date();
-    const nyTime = d.toLocaleTimeString('en-US', { timeZone: 'America/New_York', hour12: false });
+    const nyTime = d.toLocaleTimeString('en-US', { timeZone: 'America/New_York', hour12: format });
 
-    const ilTime = d.toLocaleTimeString('en-US', { timeZone: 'Asia/Jerusalem', hour12: false });
-    const ukTime = d.toLocaleTimeString('en-US', { timeZone: 'Europe/London', hour12: false });
-    const indiaTime = d.toLocaleTimeString('en-US', { timeZone: 'Asia/Kolkata', hour12: false });
+    const ilTime = d.toLocaleTimeString('en-US', { timeZone: 'Asia/Jerusalem', hour12: format });
+    const ukTime = d.toLocaleTimeString('en-US', { timeZone: 'Europe/London', hour12: format });
+    const indiaTime = d.toLocaleTimeString('en-US', { timeZone: 'Asia/Kolkata', hour12: format });
     timeIsrael.textContent = ilTime
     timeIndia.textContent = indiaTime
     timeUk.textContent = ukTime
