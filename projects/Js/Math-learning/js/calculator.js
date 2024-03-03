7234949ac1c0bf5fce2125a60c10c5fa64e57522
galeshayek <galeshayek15@gmail.com>
2024-03-03 14:11:25 +0200

    updated calculus js to be more efficient

--- projects/Js/Math-learning/js/calculator.js
+++ projects/Js/Math-learning/js/calculator.js
@@ -9,35 +9,37 @@ const table = document.getElementById("list");
 let pointsCalc = 0
 let pointsSum = 0;
 
-numRange.addEventListener('change', () => {
+function updateNumbers() {
     let value = numRange.value;
+    let num1 = 0;
+    let num2 = 0;
     switch (value) {
         case '1-10':
             num1 = Math.floor(Math.random() * 10) + 1;
-            do {
-                num2 = Math.floor(Math.random() * 10) + 1;
-            } while (num2 >= num1);
-            firstNum.innerHTML = num1
-            secondNum.innerHTML = num2
+            num2 = Math.floor(Math.random() * (num1 - 1)) + 1; // Ensure num2 < num1
+            firstNum.innerHTML = num1;
+            secondNum.innerHTML = num2;
             break;
         case '1-100':
             num1 = Math.floor(Math.random() * 100) + 1;
-            do {
-                num2 = Math.floor(Math.random() * 100) + 1;
-            } while (num2 >= num1);
-            firstNum.innerHTML = num1
-            secondNum.innerHTML = num2
+            num2 = Math.floor(Math.random() * (num1 - 1)) + 1; // Ensure num2 < num1
+            firstNum.innerHTML = num1;
+            secondNum.innerHTML = num2;
             break;
         case '1-1000':
             num1 = Math.floor(Math.random() * 1000) + 1;
-            do {
-                num2 = Math.floor(Math.random() * 1000) + 1;
-            } while (num2 >= num1);
-            firstNum.innerHTML = num1
-            secondNum.innerHTML = num2
+            num2 = Math.floor(Math.random() * (num1 - 1)) + 1; // Ensure num2 < num1
+            firstNum.innerHTML = num1;
+            secondNum.innerHTML = num2;
             break;
     }
+}
+
+
+numRange.addEventListener('change', () => {
+    updateNumbers();
 })
+
 operatorRange.addEventListener('change', () => {
     let value = operatorRange.value;
     switch (value) {
@@ -58,8 +60,8 @@ operatorRange.addEventListener('change', () => {
 })
 
 button.addEventListener("click", () => {
-    // let pointsSum = 0;
-    currectAns = +(firstNum.textContent) + +(secondNum.textContent);
+    // calculates the currect answer
+    let currectAns = +(firstNum.textContent) + +(secondNum.textContent);
     switch (true) {
         case operator.textContent == '-':
             currectAns = +(firstNum.textContent) - +(secondNum.textContent);
@@ -70,37 +72,32 @@ button.addEventListener("click", () => {
         case operator.textContent == '/':
             currectAns = +(firstNum.textContent) / +(secondNum.textContent);
             break;
-        default:
-            currectAns = +(firstNum.textContent) + +(secondNum.textContent);
     }
-
-    if (+(userInput.value) == currectAns) {
+    // updates user points
+    if (+(userInput.value) === currectAns) {
         pointsCalc = 5
         pointsSum += pointsCalc
     }
 
+    let fragment = document.createDocumentFragment();
     let div = document.createElement("div");
-
-    let targil = document.createElement("p")
+    let targil = document.createElement("p");
     targil.innerHTML = `${firstNum.textContent} ${operator.textContent} ${secondNum.textContent}`;
     div.appendChild(targil);
-
-    let p2 = document.createElement("p");
-    p2.innerHTML = currectAns;
-    div.appendChild(p2);
-
+    let paraCurrectAns = document.createElement("p");
+    paraCurrectAns.innerHTML = currectAns;
+    div.appendChild(paraCurrectAns);
     let userAns = document.createElement("p");
     userAns.innerHTML = userInput.value;
     div.appendChild(userAns);
-
     let points = document.createElement('p');
     points.innerHTML = pointsSum;
     div.appendChild(points);
+    fragment.appendChild(div);
+    table.appendChild(fragment);
 
-
-
-    table.appendChild(div);
-    let event = new Event('change');
-    numRange.dispatchEvent(event);
+    updateNumbers();
+    userInput.value = "";
 })
+
 
