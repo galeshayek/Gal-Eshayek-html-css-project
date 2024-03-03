87915c83b8280ab1e77816d178576f6d1a7c00b0
galeshayek <galeshayek15@gmail.com>
2024-03-03 21:58:22 +0200

    added calculator project

---
+++ projects/Js/calculator/js/calculator.js
@@ -0,0 +1,39 @@
+const calc = document.querySelector('.calculator');
+const invalidPlaceholder = document.querySelector('#invalid-placeholder');
+const keys = document.querySelector(".calculator-keys");
+const display = document.querySelector('.calculator-screen');
+const equal = document.querySelector('.equal-sign');
+const ac = document.querySelector('.all-clear');
+//remder clicks to the display
+keys.addEventListener('click', (click) => {
+    if (click.target.nodeName === 'BUTTON') {
+        const buttonValue = click.target.value;
+        if (buttonValue !== 'AC' && buttonValue !== '=') {
+            display.value += buttonValue;
+            console.log(buttonValue);
+        } else if (display.value == "undefined") {
+            display.value = "";
+        }
+    }
+})
+//clear display
+ac.addEventListener('click', () => {
+    display.value = "";
+})
+
+//calculate
+equal.addEventListener('click', () => {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        const div = document.createElement('div');
+        div.setAttribute('id', 'error-msg');
+        div.textContent = "Invalid Input";
+        calc.removeChild(invalidPlaceholder);
+        calc.prepend(div);
+        setTimeout(() => {
+            calc.removeChild(div);
+            calc.prepend(invalidPlaceholder);
+        }, 2000);
+    }
+})
