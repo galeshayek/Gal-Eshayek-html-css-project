9f4548d775cbaf59647b4d4292c06eb4049617c1
galeshayek <galeshayek15@gmail.com>
2024-03-11 21:08:15 +0200

    updated form

---
+++ js/form.js
@@ -0,0 +1,16 @@
+import { createAlert } from '../projects/Js/@services/utils.js'
+const fullName = document.getElementById("floatingInputName");
+const email = document.getElementById("floatingInputEmail");
+const phoneNumber = document.getElementById("floatingInputPhone");
+const textArea = document.getElementById("floatingTextarea");
+const submit = document.getElementById('submit-form');
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+})
+
+submit.addEventListener('click', () => {
+    fullName.value = "";
+    email.value = "";
+    phoneNumber.value = "";
+    textArea.value = "";
+})
